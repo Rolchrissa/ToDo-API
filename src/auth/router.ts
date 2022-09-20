8eed8ea3a896df09f0064cdb260cec747c442dc4
Christopher Roldán Sánchez <Christopher_rol@hotmail.es>
2022-09-19 21:15:28 -0300

    auth user endpoint

--- src/auth/router.ts
+++ src/auth/router.ts
@@ -1,25 +1,36 @@
 import { Router } from "express";
 import { generateAccessToken, validateToken } from "../crypto/jwt";
+import { createUser, getUser } from "../database/user";
 const router = Router();
 
-router.post("/login", (req, res) => {
+router.post("/login", async (req, res) => {
   const { username, password } = req.body;
-  // console.log]);
-
-  console.log("User: " + username, "\nPassword: " + password);
-
-  if (username === "admin" && password === "admin") {
-    const user = {
-      id: 1,
-      username: "admin",
-      email: "admin@admin.com",
+  if (!username || !password) {
+    return res.status(400).json({ message: "Bad Request" });
+  }
+  const user = await getUser(username);
+  if (!user) return res.send({ error: "user not found" }).status(404);
+  if (username === user?.username && password === user.password) {
+    const User = {
+      id: user._id,
+      username,
+      email: user.email,
+      img: user.img,
     };
-    const accessToken = generateAccessToken(user);
-    res.header("authorization", accessToken).json({
-      message: "user logged in",
-      accessToken,
-      ip: req.ip,
-    });
+    const accessToken = generateAccessToken(User);
+    res
+      .header("authorization", accessToken)
+      .json({
+        status: "success",
+        account: {
+          id: user.id,
+          username: user.username,
+          email: user.email,
+          accessToken,
+          img: user.img,
+        },
+      })
+      .status(200);
   } else {
     res.status(401).json({
       message: "Invalid credentials",
@@ -32,6 +43,36 @@ router.get("/profile", validateToken, (req, res) => {
     message: "user profile",
     user: req.user,
   });
+});
+
+router.post("/register", async (req, res) => {
+  const { username, email, password } = req.body;
+  if (!username || !email || !password) {
+    return res.status(400).json({ message: "Bad Request" });
+  }
+  const user = await getUser(username);
+  if (user) return res.json({ error: "user already exists" }).status(400);
+  let user_data = {
+    username,
+    email,
+  };
+  const token = generateAccessToken(user_data);
+  const newUser = await createUser({
+    username,
+    email,
+    password,
+    token,
+    img: "",
+  });
+  if (newUser) {
+    res.status(201).json({
+      message: "user created",
+    });
+  } else {
+    res.status(400).json({
+      message: "user not created",
+    });
+  }
 });
 
 export default router;
