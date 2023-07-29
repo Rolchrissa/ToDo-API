aa3bf714cf6ae6932984c958c6bf38ac2f0a5135
Christopher Roldán Sánchez <christopher_rol@hotmail.es>
2023-07-29 12:15:19 -0400

    hashPassword

--- src/utils/crypto/bcrypt.ts
+++ src/utils/crypto/bcrypt.ts
@@ -1,8 +1,16 @@
 import bcrypt from "bcrypt";
-const hashPassword = async (password: string) => {
+const hashPassword = async (password: string | null) => {
+  if (!password) throw new Error("Password is required");
+  const maxLength = 72;
+  const encoder = new TextEncoder(); // Utiliza TextEncoder para contar los bytes, no los caracteres
+  const encodedPassword = encoder.encode(password);
+
+  if (encodedPassword.length > maxLength) {
+    throw new Error("is to long");
+  }
+
   const salt = await bcrypt.genSalt(10);
   const hash = await bcrypt.hash(password, salt);
-  console.log("hash", hash);
   return hash;
 };
 
